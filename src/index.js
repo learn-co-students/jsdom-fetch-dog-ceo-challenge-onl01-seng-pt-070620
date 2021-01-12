a8254dce27bde58271859113d59ba04bfc563f58
julieconnors <juliana.g.connors@gmail.com>
2021-01-11 20:57:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(response => response.json())
+      .then(results => {
+        results.message.forEach(image => addImage(image))
+      });
+  }
+
+  function addImage(dogImgUrl) {
+    let container = document.querySelector('#dog-image-container');
+    let newImage = document.createElement('img');
+    newImage.src = dogImgUrl;
+    container.appendChild(newImage);
+  }
