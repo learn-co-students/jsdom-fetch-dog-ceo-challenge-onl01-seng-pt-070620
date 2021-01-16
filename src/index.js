64e137481ca4a3c22b063ea7db81ba6e9e26b6e2
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-01-16 16:01:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,69 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4",
+     breedUrl = 'https://dog.ceo/api/breeds/list/all',
+     div = document.getElementById('dog-image-container'),
+     ul = document.getElementById('dog-breeds'),
+     dropDown  = document.getElementById('breed-dropdown');
+
+    fetchImages();
+    fetchBreeds();
+
+    function fetchImages() {
+        return fetch(imgUrl)
+        .then((response) => response.json())
+        .then(json => renderImages(json));
+      }
+    
+      function fetchBreeds() {
+        return fetch(breedUrl)
+        .then((response) => response.json())
+        .then(json => renderBreeds(json));
+      }
+      
+      function renderImages(images) {
+        images.message.forEach(image => {
+          const img = document.createElement('img')
+          img.src = image
+          div.appendChild(img)
+        })
+      }
+    
+      function renderBreeds(breeds) {
+        Object.keys(breeds.message).forEach(breed => {
+          const li = document.createElement('li');
+          li.innerText = breed;
+        //   li.id = breed;
+          ul.appendChild(li);
+        })
+      }
+
+      ul.addEventListener('click', function(e){
+        e.target.style.color == 'red' ? e.target.style.color = 'black' : e.target.style.color = 'red';
+      });
+
+      dropDown.addEventListener('change', function(e){
+          const letter = e.target.value;
+          var items = ul.getElementsByTagName("li");
+          for (var i = 0; i < items.length; ++i) {
+                if (items[i].innerText.charAt(0) != letter){
+                    items[i].hidden = true;
+                }
+                else {
+                    items[i].hidden = false;
+                }
+          }
+        
+      });
+ })
+
+
+
+
+
+  
+ 
+  
+  
+  
