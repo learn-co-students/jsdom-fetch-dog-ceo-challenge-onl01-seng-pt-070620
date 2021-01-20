80b53d39b30c6f5c4fb514bb19a7d2f54bf67d0e
Raveen K Sharma <raveen08@gmail.com>
2021-01-20 21:14:35 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImg() {
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+return fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => renderImg(json));
+}
+
+
+function renderImg(images) {
+  const contain = document.querySelector('dog-image-container')
+  images.foreach(img =>{
+  let newImage = document.createElement('img');
+  newImage.innerHTML = `<img src="${img}">`
+  contain.appendChild(newImage);
+  
+})}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchImg()
+})
